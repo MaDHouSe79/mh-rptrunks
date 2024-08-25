05411eccdbec2154510bc9eca60cddd9ab05ad00
MaDHouSe <madhouse1979@gmail.com>
2024-08-25 23:04:42 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,21 +6,17 @@ local QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent('mh-rptrunks:server:refreshConfig')
 AddEventHandler('mh-rptrunks:server:refreshConfig', function()
     local src = source
-    TriggerClientEvent('mh-rptrunks:client:sendConfig', src, SV_Config.Vehicles, SV_Config.Animations)
+    TriggerClientEvent('mh-rptrunks:client:sendConfig', src, SV_Config.Vehicles)
 end)
 
 RegisterNetEvent('entityCreating')
 AddEventHandler('entityCreating', function(entity)
     if entity ~= nil and DoesEntityExist(entity) then
-        local found, doors = false, {}
+        local netid = NetworkGetNetworkIdFromEntity(entity)
+        local found, doors = false, nil
         for k, v in pairs(SV_Config.Vehicles) do
-            if GetHashKey(k) == GetEntityModel(entity) then
-                found, doors = true, v.doors
-            end
+            if GetHashKey(k) == GetEntityModel(entity) then found, doors = true, v.doors end
         end
-        if found then
-            local netid = NetworkGetNetworkIdFromEntity(entity)
-            TriggerClientEvent('mh-rptrunks:client:CreateVehicleTarget', -1, netid, doors)
-        end
+        if found then TriggerClientEvent('mh-rptrunks:client:CreateVehicleTarget', -1, netid, doors) end
     end
 end)
